d3550ef92d4849548959613ac806649e6f6cf77e
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2018-07-02 17:10:06 +0100

    scoreboard

--- src/Scoreboard/index.js
+++ src/Scoreboard/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import './scoreboard.css'
+import hbkScoreboard from './hbk_scoreboard.png'
+
 const Scoreboard = () => (
-  <div>
-    <h2>Scoreboard</h2>
+  <div className='root'>
+    <span className='rawline-bold white Scoreboard-text-hbk'>HBK<span className='orange'>#13</span></span>
+    <span className='raleway-bold white Scoreboard-text-brewdog'><span className='orange'>BREWDOG</span> BRIGHTON</span>
+    <span className='raleway-bold white Scoreboard-text-p1s'>3</span>
+    <span className='raleway-bold white Scoreboard-text-p2s'>3</span>
+    <span className='rawline-bold white Scoreboard-text-p1n'>ColdLink Sensei</span>
+    <span className='rawline-bold white Scoreboard-text-p2n'>ColdLink Sensei</span>
+    <img src={hbkScoreboard} className='Scoreboard-main' alt='camera' />
   </div>
 )
 
